3ca8fcea9d091a73620fdd14d3005b7a271baa5e
Emanuele <Emanuele@Danilo-Desktop.deis.unibo.it>
2016-01-14 14:02:31 +0100

    Due decimali. Timer delay.

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -6,14 +6,14 @@
 main_SHT21.obj: ../main_SHT21.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmspx --abi=eabi --data_model=restricted -Ooff --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power=all --advice:hw_config=all -g --define=__MSP430FR5969__ --define=_MPU_ENABLE --diag_warning=225 --display_error_number --diag_wrap=off --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --printf_support=minimal --preproc_with_compile --preproc_dependency="main_SHT21.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/tiv6_2/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmspx --abi=eabi --data_model=restricted -Ooff --include_path="C:/tiv6_2/ccsv6/ccs_base/msp430/include" --include_path="C:/tiv6_2/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power="all" --advice:hw_config="all" -g --define=__MSP430FR5969__ --define=_MPU_ENABLE --diag_warning=225 --display_error_number --diag_wrap=off --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --printf_support=full --preproc_with_compile --preproc_dependency="main_SHT21.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 shtLib.obj: ../shtLib.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmspx --abi=eabi --data_model=restricted -Ooff --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power=all --advice:hw_config=all -g --define=__MSP430FR5969__ --define=_MPU_ENABLE --diag_warning=225 --display_error_number --diag_wrap=off --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --printf_support=minimal --preproc_with_compile --preproc_dependency="shtLib.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/tiv6_2/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/bin/cl430" -vmspx --abi=eabi --data_model=restricted -Ooff --include_path="C:/tiv6_2/ccsv6/ccs_base/msp430/include" --include_path="C:/tiv6_2/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include" --advice:power="all" --advice:hw_config="all" -g --define=__MSP430FR5969__ --define=_MPU_ENABLE --diag_warning=225 --display_error_number --diag_wrap=off --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --printf_support=full --preproc_with_compile --preproc_dependency="shtLib.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- main_SHT21.c
+++ main_SHT21.c
@@ -28,6 +28,7 @@
 #include <string.h>
 #include <stdio.h>
 
+#define ID 1
 
 /**
  * Initialize UCA0 module in UART mode with boud rate 9600
@@ -81,13 +82,99 @@ void sendByteUARTA1(unsigned char byte)
 	UCA1TXBUF = byte;
 }
 
-char buffer[20];
-int i=0, c=0;
+void gpioLowPower()
+{
+	 // Configure GPIO
 
-// Main ----------------------------------------------------------------------------------------------
-int main(void) {
-	WDTCTL = WDTPW | WDTHOLD;       // Stop WDT
+	// -------- BOARD PROFONDITA' --------
+	P1OUT = 0xFF;
+	P1DIR = 0xFF;
 
+	P2OUT = 0xFF;
+	P2DIR = 0xFF;
+
+	P3OUT = 0xFF;
+	P3DIR = 0xFF;
+
+	P4OUT = 0xFF;
+	P4DIR = 0xFF;
+
+	PJOUT = 0xFF;
+	PJDIR = 0xFF;
+
+	// Shout down mode
+	P3OUT &= ~BIT2;					// DE enable low
+	P3OUT |= BIT3;					// !RE enable hight
+	P3DIR |= BIT2 + BIT3;			// 3.2 = DE, 3.3 = RE
+
+	// LED power off
+	P3OUT &= ~BIT7;
+	P3DIR |= BIT7;
+
+	//    // -------- BOARD SUPERFICE --------
+	//  P1OUT = 0x00;
+	//  P1OUT = BIT1 + BIT2 + BIT6 + BIT7;
+	//  P1DIR = 0x00;
+	//  P1DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5;
+	//
+	//  P2OUT = 0x00;
+	//  P2OUT = BIT2 + BIT4 + BIT5 + BIT6 + BIT7;
+	//  P2DIR = 0x00;
+	//  P2DIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+	//
+	//  P3OUT = 0xFF;
+	//  P3DIR = 0xFF;
+	//
+	//  P4OUT = 0x00;
+	//  P4OUT = BIT6 + BIT7;
+	//  P4DIR = 0x00;
+	//  P4DIR = BIT6 + BIT7;
+	//
+	//  PJOUT = 0x00;
+	//  PJOUT = BIT0 + BIT1 + BIT2 + BIT4 + BIT5 + BIT6 + BIT7;
+	//  PJDIR = BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+
+	//	// Shout down mode
+	//	P3OUT &= ~BIT2;					// DE enable low
+	//	P3OUT |= BIT3;					// !RE enable hight
+	//	P3DIR |= BIT2 + BIT3;			// 3.2 = DE, 3.3 = RE
+	//
+	//	// LED power off
+	//	P3OUT &= ~BIT7;
+	//	P3DIR |= BIT7;
+	//
+	//	// Radio off
+	//	P1OUT |= BIT2;
+	//	P1DIR |= BIT2;
+	//
+	//	// ADCEN off
+	//	P2OUT |= BIT2;
+	//	P2DIR |= BIT2;
+	//
+	//	// ADC level low
+	//	P2OUT &= ~BIT3;
+	//	P2DIR |= BIT3;
+	//
+	//	// -------- eink --------
+	//	// #RST
+	//	P1OUT &= ~BIT1;
+	//	P1DIR |= BIT1;
+	//	// PWM, DCH
+	//	P2OUT |= BIT4;
+	//	P2OUT &= ~BIT7;
+	//	P2DIR &= BIT7;
+	//	// #DCS, BUSY
+	//	P3OUT |= BIT0;
+	//	P3OUT &= ~BIT1;
+	//	P3DIR |= BIT0;
+	//	P3DIR &= ~BIT0;
+	//	// #RST
+	//	PJOUT |= BIT0;
+	//	PJDIR |= BIT0;
+}
+
+void gpioInit()
+{
 	// Configure GPIO
 	P1OUT &= ~BIT0;                           // Clear P1.0 output latch
 	P3DIR |= BIT7;                            // For LED
@@ -95,6 +182,16 @@ int main(void) {
 
 	P3DIR |= BIT2;		// 3.2 = DE, 3.3 = RE
 	P3OUT |= BIT2;		// DE enable hight, RE hight-impedence
+}
+
+unsigned char buff[50];
+volatile unsigned int i=0, c=0;            // volatile to prevent optimization
+
+// Main ----------------------------------------------------------------------------------------------
+int main(void) {
+	WDTCTL = WDTPW | WDTHOLD;       // Stop WDT
+
+	gpioLowPower();
 
 	// Disable the GPIO power-on default high-impedance mode to activate
 	// previously configured port settings
@@ -110,53 +207,36 @@ int main(void) {
 	// Lock CS registers - Why isn't PUC created?
 	CSCTL0_H = 0;
 
-	initUARTA0();
-	initUARTA1();
-
 	// Enable interrupts
 	__bis_SR_register(GIE);
 
 	__delay_cycles(10000);
 
-//	  g_temp = 0.0f;
-//	  g_hum = 0.0f;
-//
-//	  // Read Temperature
-//	  SHT21ReadTemperature();
-//
-//	  // Read Humidity
-//	  SHT21ReadHumidity();
-//
-//	  g_temp = g_temp;
-//
-//	  g_hum = g_hum;
-
-	//char buff[12] = {'C','i','a','o',' ','m','o','n','d','o','\n','\0'};
-	unsigned char buff[50];
-	int i = 0;
-	int d1 = 0;
-	float f2 = 0;
-	int d2 = 0;
-
 	for(;;) {
 		volatile unsigned int i;            // volatile to prevent optimization
+
+		gpioInit();
+		P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
 
 		// Read Temperature
 		SHT21ReadTemperature();
 //		// Read Humidity
 //		SHT21ReadHumidity();
 
-		d1 = g_temp;            // Get the integer part (678).
-		f2 = g_temp - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
-		d2 = (int)(f2 * 10000); // Or this one: Turn into integer.
-
 		buff[0]='\0';
-		sprintf(buff, "T:%d.%d\n", d1, d2);
+		sprintf(buff, "T%d:%.2f\n", ID,  g_temp);
+
+		initUARTA0();
+		initUARTA1();
+
 		for(i=0;i<strlen(buff);i++)
 		{
 			sendByteUARTA0(buff[i]);
 			sendByteUARTA1(buff[i]);
 		}
+
+		P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
+		gpioLowPower();
 
 //		d1 = g_hum;            // Get the integer part (678).
 //		f2 = g_hum - d1;     // Get fractional part (678.0123 - 678 = 0.0123).
@@ -169,14 +249,13 @@ int main(void) {
 //			sendByteUARTA1(buff[i]);
 //		}
 
-		P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
+//		P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
 
-		__delay_cycles(1000000);
-		__delay_cycles(1000000);
-		__delay_cycles(1000000);
-		__delay_cycles(1000000);
-		__delay_cycles(1000000);
-		__delay_cycles(1000000);
+		// Inizializzazione del timer
+		TA0CCTL0 = CCIE;                    // TACCR0 interrupt enabled
+		TA0CCR0 = 62500 - 1;
+		TA0CTL = TASSEL__SMCLK | MC__CONTINOUS;   // SMCLK, continuous mode
+		__bis_SR_register(LPM4_bits + GIE);       // Enter LPM3 w/ interrupt
 	}
 }
 
@@ -199,6 +278,13 @@ void __attribute__ ((interrupt(TIMER0_B0_VECTOR))) Timer0_B0_ISR (void)
 	__bic_SR_register_on_exit(LPM3_bits); 	// Exit LPM3
 }
 
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0 (void) {		// Timer0 A0 interrupt service routine
+	 if (++c == 50) {
+			__bic_SR_register_on_exit(LPM4_bits); 	// Exit LPM3
+			c=0;
+	}
+}
 
 //#pragma vector=USCI_A1_VECTOR
 //__interrupt void USCI_A1_ISR(void)
